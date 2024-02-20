f0bed874177e1ecc28564d98e67f2d8f90f24408
BilelHimeur <bilelhimeur@gmail.com>
2024-02-20 17:02:59 +0100

    Commit initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ma-piece-front';
+  title = 'Ma pièce à moi par AUTODISTRIBUTION';
 }
