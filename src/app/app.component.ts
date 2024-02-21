7de0958154e36db45d221ec32ef7fa7846818f28
BilelHimeur <bilelhimeur@gmail.com>
2024-02-21 14:27:08 +0100

    suppression ancien code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import {Component, ViewChild} from '@angular/core';
 import {AppModule} from "./app.module";
 import {FormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
-import {VehiculeDetailsComponent} from "./vehicule-details/vehicule-details.component";
 import {VehiculeService} from "./vehicule.service";
 import {VehiculeDetails} from "./vehicule-details";
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
-import {VehiculeDetailsComponent} from "./vehicule-details/vehicule-details.component";
 import {VehiculeService} from "./vehicule.service";
 import {CommonModule} from "@angular/common";
 
@@ -14,12 +13,10 @@ import {CommonModule} from "@angular/common";
     CommonModule,
   ],
   declarations: [
-    VehiculeDetailsComponent,
   ],
   providers: [VehiculeService], // Provide your services here
   bootstrap: [AppComponent],
   exports: [
-    VehiculeDetailsComponent
   ]
 })
 export class AppModule {
